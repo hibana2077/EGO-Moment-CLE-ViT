122a4e4ac38ee181fcb7cdcce2062a3606a7d294
hibana2077 <hibana2077@gmail.com>
2025-08-04 16:14:07 +0800

    fix: Increase memory allocation to 32GB in training script

--- scripts/train_a100.sh
+++ scripts/train_a100.sh
@@ -3,7 +3,7 @@
 #PBS -q dgxa100
 #PBS -l ngpus=1            
 #PBS -l ncpus=16            
-#PBS -l mem=20GB           
+#PBS -l mem=32GB           
 #PBS -l walltime=00:60:00  
 #PBS -l wd                  
 #PBS -l storage=scratch/rp06
